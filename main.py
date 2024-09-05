1fba00587f2aaf387150490b8ca0f21832ae4ba8
TokeHOlesen <tokeholesen@gmail.com>
2024-09-05 13:04:01 +0200

    SimulatedKid class added; Code broken up

--- main.py
+++ main.py
@@ -4,127 +4,14 @@ import sys
 from datetime import datetime, timedelta, date
 from pathlib import Path
 
+from OrderlineClass import Orderline
+from DayClass import Day
+from AlldaysClass import Alldays
+from SimulatedKidClass import SimulatedKid
+
 SQL_DB_PATH = Path(r"C:\Users\Toke Henrik Olesen\Code\PalissadeDB\sqldb.db")
 EXCEL_FILE_PATH = Path(r"palmod.xlsx")
 
-
-class Alldays:
-    SHIPPING_DAYS = {
-    "BG": (0, ),
-    "CH": (2, 4),
-    "CZ": (0, 3),
-    "EE": (0, 3),
-    "ES": (0, 2, 4),
-    "FI": (2, 4),
-    "FR": (0, 3, 4),
-    "GR": (3, ),
-    "HR": (3, ),
-    "HU": (0, 3),
-    "IT": (0, 2, 4),
-    "LT": (0, 3),
-    "LV": (0, 3),
-    "NO": (3, ),
-    "PL": (0, 3),
-    "PT": (0, 3),
-    "RO": (2, ),
-    "SI": (3, ),
-    "SK": (0, 3),
-    "SM": (0, 2, 4),
-    "TR": (3, )
-}
-    
-    def __init__(self) -> None:
-        self._days = []
-        
-    def __iter__(self):
-        return iter(self._days)
-    
-    def get_day(self, index):
-        return self.days[index]
-    
-    def add_day(self, day):
-        self._days.append(day)
-    
-    def move_lines_according_to_date(self):
-        """
-        If an orderline's date falls on a day on which orders to the orderline's country may not be shipped,
-        moves the orderline back to the nearest allowed date.
-        If the orderline is moved all the way back to monday without reaching an allowed day, the orderline's
-        "message" property is set to "Forsinket" to mark is as delayed.
-        """
-        for day in self._days:
-            for country in day.countries:
-                weekday = target_weekday = day.weekday_number
-                if country in self.SHIPPING_DAYS:
-                    if weekday not in self.SHIPPING_DAYS[country]:
-                        while target_weekday not in self.SHIPPING_DAYS[country] and target_weekday > 0:
-                            target_weekday -= 1
-                        for orderline in day.orderlines[:]:
-                            if orderline.country == country:
-                                if target_weekday == 0 and target_weekday not in self.SHIPPING_DAYS[country]:
-                                    orderline.message = "Forsinket"
-                                self._days[target_weekday].add_line(orderline)
-                                day.remove_line(orderline)
-
-
-class Day:
-    def __init__(self, date) -> None:
-        self._date = date
-        self._orderlines = []
-         
-    @property    
-    def orderlines(self):
-        return self._orderlines
-    
-    def add_line(self, orderline):
-        self._orderlines.append(orderline)
-    
-    def remove_line(self, orderline):
-        self._orderlines.remove(orderline)
-
-    @property
-    def number_of_lines(self):
-        return len(self._orderlines)
-    
-    @property
-    def date(self):
-        return self._date
-
-    @property
-    def weekday_number(self):
-        return self._date.weekday()
-    
-    @property
-    def weekday(self):
-        weekdays = ["Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag"]
-        return weekdays[self.weekday_number % 7]
-
-    @property
-    def countries(self):
-        return list({orderline.country for orderline in self.orderlines})
-
-
-class Orderline:
-    def __init__(self, sql_data) -> None:
-        (self.id,
-        self.ordernumber,
-        self.pickseries,
-        self.location,
-        self.itemnumber,
-        self.itemname,
-        self.color,
-        self.number,
-        self.loadmeter,
-        self.date,
-        self.kid,
-        self.custname,
-        self.address,
-        self.city,
-        self.postcode,
-        self.country) = sql_data
-    
-        self.message = ""
-    
 
 def main():
     # TODO: exceptions
@@ -165,13 +52,19 @@ def main():
     load_data_into_db(cursor, df)
     
     all_days = get_orders_for_week(cursor, '2024-09-09', '2024-09-13')
-    all_days.move_lines_according_to_date()
+    all_days.move_lines_to_match_date()
+    all_days.calculate_kids_for_all_days()
     
     for day in all_days:
         print("\n", day.date, day.weekday, "\n")
         for orderline in day.orderlines:
-            print(orderline.country, orderline.date, orderline.address, orderline.message)
+            print(orderline.ordernumber, orderline.country, orderline.date, orderline.address, orderline.message)
     
+    for day in all_days:
+        for kid in day.kids:
+            print(kid.address, kid.ordernumbers, kid.number, kid.ldm)
+    
+        
     cursor.close()
     connection.commit()
     connection.close()
